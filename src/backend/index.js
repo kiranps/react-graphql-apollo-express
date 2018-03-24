9e4ce501e2e0d7b83dce2a4ee0ff5d8f8fe5119e
Kiran PS <pskirann@gmail.com>
2018-03-24 11:55:40 +0530

    Added static pages

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/react', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(3004, () => console.log('Example app listening on port 3000!'))
